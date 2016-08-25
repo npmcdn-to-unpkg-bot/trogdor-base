1a3b504820ac0e454a0eaed70d90d8fe28bee059
crmaykish <crmaykish@gmail.com>
2016-08-25 03:33:28 +0000

    Added center marker to map

--- raspi/webservice/static/script.js
+++ raspi/webservice/static/script.js
@@ -7,11 +7,15 @@ $(document).ready(function(){
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
+    var centerMarker = L.marker([0,0]);
+    centerMarker.addTo(map);
+
     socket.on('gps', function(message) {
         console.log(message);
         $('#time').html("Time (UTC): " + message.time);
         $('#coords').html(message.latitude + ', ' + message.longitude);
         map.setView([message.latitude, message.longitude], 18);
+        centerMarker.setLatLng([message.latitude, message.longitude]);
     });
 
     socket.on('satellites', function(message) {
