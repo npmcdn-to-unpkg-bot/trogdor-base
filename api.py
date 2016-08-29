82718383c09788d1b6c6ad8f87ac3ce25572118c
crmaykish <crmaykish@gmail.com>
2016-08-29 01:26:54 +0000

    Moving to raspi3

--- api.py
+++ api.py
@@ -3,13 +3,19 @@ import motor
 from threading import Thread
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import logging
+from lib.Xbox import xbox
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app, async_mode='threading')
 
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
 gps_thread = None
 motor_thread = None
+joystick_thread = None
 
 m = None
 
@@ -30,6 +36,16 @@ def gps_loop():
         g.parse()
         socketio.emit('gps', g.get_json())
 
+def joystick_loop():
+    joy = xbox.Joystick()
+    print("Xbox controller connected: " + str(joy.connected()))
+    while True:
+        left = joy.leftY()
+        right = joy.rightY()
+        left_mapped = left * 255
+        right_mapped = right * 255
+        print(str(left_mapped) + ", " + str(right_mapped))
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -48,14 +64,17 @@ def stop():
 
 @socketio.on('connect')
 def connected():
-    print("connected")
-    global gps_thread
-    if gps_thread is None:
-        gps_thread = socketio.start_background_task(target=gps_loop)
-
-    global motor_thread
-    if motor_thread is None:
-        motor_thread = socketio.start_background_task(target=motor_loop)
+    print("Client connected.")
+    # global gps_thread
+    # if gps_thread is None:
+    #     gps_thread = socketio.start_background_task(target=gps_loop)
+    #
+    # global motor_thread
+    # if motor_thread is None:
+    #     motor_thread = socketio.start_background_task(target=motor_loop)
+    global joystick_thread
+    if joystick_thread is None:
+        joystick_thread = socketio.start_background_task(target=joystick_loop)
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", debug=True)
